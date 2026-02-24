e2441b45cc7c92416c6c12b2c4201eb68f708844
Syuraih Umar Khotthob <syuraihumar@gmail.com>
2026-02-24 14:41:33 +0700

    Realisasi method-method dalam class Titik

--- Pertemuan2/src/MTitik.java
+++ Pertemuan2/src/MTitik.java
@@ -1,4 +1,4 @@
-// Nama File   : Titik.java
+// Nama File   : MTitik.java
 // Deskripsi   : berisi atribut dan method dalam class Titik
 // Pembuat     : Syuraih Umar Khotthob 
 // Tanggal     : 24 Februari 2026
@@ -15,5 +15,29 @@ public class MTitik {
         T1.geser(3, 4);         // menggeser T1 sejauh (3,4)
         
         T1.printTitik();        // menampilkan koordinat T1 setelah digeser
+
+        Titik T2 = T1;
+        T2.printTitik();
+        T1.setAbsis(10);
+        T1.setOrdinat(10);
+        T2.printTitik();
+
+        Titik T3 = new Titik(15.0,15.0);
+        T3.printTitik();
+
+        Titik T4 = new Titik(20.0,20.0);
+        System.out.println("Jumlah Objek Titik = " + Titik.getCounterTitik());
+        System.out.println("Jumlah Objek Titik = " + T3.getCounterTitik());
+        System.out.println("Kuadran T4 = " + T4.getKuadran());
+        System.out.println("Jarak T4 dengan titik pusat = " + T4.getJarakPusat());
+        System.out.println("Jarak T4 dengan T3 = " + T4.getJarak(T3));
+        T4.refleksiX();
+        T4.printTitik();
+
+        Titik T5 = new Titik(6.0,17.0);
+        T5.printTitik();
+        Titik T5RefleksiX = T5.getRefleksiX();
+        System.out.println("Titik baru hasil refleksi T5 terhadap X : ");
+        T5RefleksiX.printTitik();
     }
 }
--- Pertemuan2/src/Titik.java
+++ Pertemuan2/src/Titik.java
@@ -7,12 +7,21 @@ public class Titik {
     /***************** ATRIBUT *****************/
     double absis;
     double ordinat;
+    static int counterTitik = 0;
 
     /***************** METHOD *****************/
     // konstruktor untuk membuat titik (0,0)
     Titik() {
         absis = 0;
         ordinat = 0;
+        counterTitik++;
+    }
+
+    // konstruktor untuk membaut titik dengan input x sebagai absis dan input y sebagai ordinat
+    Titik(double x, double y) {
+        absis = x;
+        ordinat = y;
+        counterTitik++;
     }
 
     // mengembalikan nilai absis
@@ -23,6 +32,11 @@ public class Titik {
     // mengembalikan nilai ordinat
     double getOrdinat() {
         return ordinat;
+    }
+
+    // mengembalikan nilai counterTitik
+    static int getCounterTitik() {
+        return counterTitik;
     }
 
     // mengeset absis titik dengan nilai baru x
@@ -41,8 +55,62 @@ public class Titik {
         ordinat = ordinat + y;
     }
 
+    // mengembalikan kuadran titik berada
+    int getKuadran() {
+        if (absis > 0 && ordinat > 0) {
+            return 1;
+        }
+        else if (absis < 0 && ordinat > 0) {
+            return 2;
+        }
+        else if (absis < 0 && ordinat < 0) {
+            return 3;
+        }
+        else if (absis > 0 && ordinat < 0) {
+            return 4;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    // mengembalikan jarak titik dengan titik pusat (0.0)
+    double getJarakPusat() {
+        return Math.sqrt(Math.pow(absis, 2) + Math.pow(ordinat, 2));
+    }
+
+    // mengembalikan jarak antara dua titik
+    double getJarak(Titik T) {
+        return Math.sqrt(Math.pow(T.getAbsis() - this.getAbsis(), 2) + Math.pow(T.getOrdinat() - this.getOrdinat(), 2));
+    }
+
+    // merefleksi titik dengan sumbu X
+    void refleksiX() {
+        ordinat = ordinat * -1.0;
+    }
+
+    // merefleksi titik dengan sumbu Y
+    void refleksiY() {
+        absis = absis * -1.0;
+    }
+
+    // mengembalikan titik baru hasil dari refleksi sebuah titik dengan sumbu X
+    Titik getRefleksiX() {
+        Titik T1 = new Titik(absis, ordinat * -1);
+
+        return T1;
+    }
+
+    // mengembalikan titik baru hasil dari refleksi sebuah titik dengan sumbu Y
+    Titik getRefleksiY() {
+        Titik T1 = new Titik(absis * -1, ordinat);
+
+        return T1;
+    }
+
     // mencetak koordinat titik
     void printTitik() {
         System.out.println("Titik (" + absis + "," + ordinat + ")");
     }
+
 } // end class Titik
